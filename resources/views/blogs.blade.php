ab30cd5a8a7e0acc35ed384583cce3347897bc75
ZinMgNyunt <zinmgnyunt99@gmail.com>
2021-12-18 20:25:17 +0630

    episode-24 Eloquent Update and Delete

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,8 +7,10 @@
         <div class="blog">
             <h1><a href="/blogs/{{ $blog->id }}"> {{  $blog->title }} </a></h1>
             <div>
-                <p>Published at - {{  $blog->date }}</p>
+                <p>Published at - {{  $blog->created_at->diffForHumans() }}</p>
+                {{-- diffForHumans() က အချိန်ကို လူတွေဖတ်လို့လွယ်အောင် laravel က လုပ်ပေးတဲ့ method --}}
                 <p>{{ $blog->intro }}</p>
+                <p>Updated at - {{ $blog->updated_at->diffForHumans() }}</p>
             </div>
         </div>
     @endforeach
