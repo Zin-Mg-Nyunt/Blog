0ca54cdcce9e89baa2399728096cd9580c4a7798
ZinMgNyunt <zinmgnyunt99@gmail.com>
2021-12-24 22:56:45 +0630

    episode-30 lazy loading or eager load and N+1 problem and clockwork

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4,10 +4,13 @@
     </x-slot>
 
     @foreach($blogs as $blog)
+    {{-- loop ပတ်တဲ့အချိန်မှာ query တစ်ခါrun --}}
         <div class="blog">
             <h1><a href="/blogs/{{ $blog->slug }}"> {{  $blog->title }} </a></h1>
-            <a href="/categories/{{ $blog->category->slug }}">{{ $blog->category->name }}</a>
-            {{-- loop ပတ်ထားတာမို့လို့ blog တွေက တစ်ခုချင်းစီ loop ပတ်ပြီး ဝင်လာနေတာမို့လို့ သက်ဆိုင်ရာ blog ရဲ့ category တွေကလဲ တစ်လှည့်စီဝင်လာမယ် --}}
+            <a href="/categories/{{ $blog->category->slug }}">
+                {{ $blog->category->name }}
+    {{-- Eloquent Relationship သုံးတဲ့အခါမှာ loop ပတ်လို့ရတဲ့ data ရှိသလောက် queryက ထပ်run။ အဲ့တော့ preform လျော့စေတယ်။အဲ့လိုမဖြစ်အောင် loop ရဲ့အပြင်မှာ Eloquent Relationship ကိုသုံးထားလိုက်မယ်ဆိုရင် query စုစုပေါင်းမှ နှစ်ခါပဲ run စရာလိုတော့မယ် --}}
+            </a>
             <div>
                 <p>Published at - {{  $blog->created_at->diffForHumans() }}</p>
                 <p>{{ $blog->intro }}</p>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Blog;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +18,17 @@ use App\Models\Category;
 */
 
 Route::get('/', function () {
+    // DB::listen(function ($query) {
+    //     // Log::info("message"); //အဲ့တာကို ရေးရလွယ်အောင် logger ပုံစံနဲ့ ရေး
+
+    //     logger($query->sql); //logger ထဲမှာ message ကိုရေးရ၊ run သွားတဲ့ query တွေကို sql ပုံစံနဲ့ ပြပါလို့ ရေးထားတာ။(/ routes) ကို refreach လုပ်လိုက်ရင် storage ထဲက logs ထဲက laravel.log ဖိုင်ထဲမှာ sql ပုံစံနဲ့ ပေါ်တယ်။
+    // });
+
+    //အဲ့လို log file ထဲမှာ ပေါ်စေချင်တိုင်း အဲ့လိုကြီးရေးနေရတာကို မလုပ်ချင်လို့ clockwork ဆိုတဲ့ tool ကို project folder ထဲမှာ install တင်(php clockwork ဆိုပြီး chrome မှာရှာ)။အဲ့လိုဆိုရင် larvel.log ဖိုင်ထဲမှာသွားကြည့်စရာမလိုတော့ဘူး။ chrome မှာ inspect ထောက်ပြီးကြည့်လိုက်ရုံပဲ
+
     return view('blogs', [
-        'blogs'=>Blog::all()
+        'blogs'=>Blog::with('category')->get()// lazy loading// eager load
+        //query run တာကို လျော့ချဖို loop ပတ်ဖို့ data ပေးလိုက်တည်းက category(Eloquent Relationship) ကိုပါထုတ်ပြီးထည့်ပေး။ with() နဲ့ category(Eloquent Relationship) ကိုထုတ်ပေးရင် allကိုသုံးလို့မရတော့၊ get()ကိုပဲသုံးရတယ်။ all() က (Blog::al()) အဲ့လိုပုံစံတစ်မျိုးတည်းပဲသုံးရတယ်။
     ]);
 });
 Route::get('/blogs/{blog:slug}', function (Blog $blog) {
