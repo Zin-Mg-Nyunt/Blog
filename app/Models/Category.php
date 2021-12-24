d1a42028d1abdffbc12a552077c3acd045204ebe
ZinMgNyunt <zinmgnyunt99@gmail.com>
2021-12-24 20:17:32 +0630

    episode-29 hasMany Relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);// Category တစ်ခုမှာ blog တွေအများကြီး ရှိတယ်။အဲ့ blog တွေအများကြီးကို ချိတ်ဖို့ hasMany ကိုသုံးရတယ်
+    }
 }
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -6,7 +6,7 @@
     @foreach($blogs as $blog)
         <div class="blog">
             <h1><a href="/blogs/{{ $blog->slug }}"> {{  $blog->title }} </a></h1>
-            <a href="">{{ $blog->category->name }}</a>
+            <a href="/categories/{{ $blog->category->slug }}">{{ $blog->category->name }}</a>
             {{-- loop ပတ်ထားတာမို့လို့ blog တွေက တစ်ခုချင်းစီ loop ပတ်ပြီး ဝင်လာနေတာမို့လို့ သက်ဆိုင်ရာ blog ရဲ့ category တွေကလဲ တစ်လှည့်စီဝင်လာမယ် --}}
             <div>
                 <p>Published at - {{  $blog->created_at->diffForHumans() }}</p>
