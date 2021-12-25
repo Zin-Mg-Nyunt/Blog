a23e85d11448b31611011f77d6e070ae118b347d
ZinMgNyunt <zinmgnyunt99@gmail.com>
2021-12-25 15:00:48 +0630

    episode-31 db seeding

--- database/migrations/2021_12_22_155427_create_categories_table.php
+++ database/migrations/2021_12_22_155427_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();//unique() က name column ထဲမှာ တူညီတဲ့ data ကိုထပ်ထည့်လို့ မရအောင် လုပ်ပေးတာ။ ထည့်ရင် error ပြမယ်။
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //truncate() က databas ထဲက အရင် data တွေကို ဖျက်တာ
+        //truncate ခံလိုက်တော့ php artisan migrate:fresh --seed ကို run လိုက်တဲ့အခါတိုင်း unique error က ထပ်မတက်တော့ဘူး
+        User::truncate();
+        Category::truncate();
+        Blog::truncate();
+
+        User::factory()->create();
+        //factory() က user တွေကို generate စီပြီး table ထဲထည့်ပေးတာ။ factory() ထဲမှာ ဘာမှ မရေးထားရင် default အနေနဲ့ 1 ကို laravel ကသတ်မှတ်ပေးထားတယ်။
+
+        $frontend=Category::create([
+            'name'=>'frontend post',
+            'slug'=>'frontend'
+        ]);
+        $backend=Category::create([
+            'name'=>'backend post',
+            'slug'=>'backend'
+        ]);
+
+        Blog::create([
+            'title'=>'Frontend Post',
+            'slug'=>'frontend-post',
+            'category_id'=>$frontend->id,
+            'intro'=>'this is intro',
+            'body'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet libero assumenda harum, sint autem laborum, facilis dolorem, voluptatum nam odio sed? Architecto illum molestiae incidunt iusto repudiandae tempora odit, excepturi quam porro consequatur quis, pariatur nulla fugiat illo dignissimos dolore aut laborum deleniti est totam repellendus suscipit recusandae libero. Sapiente eum beatae nobis pariatur praesentium? Architecto nostrum dolores iusto quo hic nemo suscipit similique maiores pariatur consequuntur. Deserunt dolorum nulla reiciendis repudiandae! Tempore soluta asperiores voluptatem reiciendis incidunt fugit libero ipsum dignissimos, excepturi aliquid, cupiditate, eum delectus tenetur quae necessitatibus inventore facilis expedita atque explicabo. Inventore dolorum nesciunt harum dolore.'
+        ]);
+        Blog::create([
+            'title'=>'Backend Post',
+            'slug'=>'backend-post',
+            'category_id'=>$backend->id,
+            'intro'=>'this is intro',
+            'body'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet libero assumenda harum, sint autem laborum, facilis dolorem, voluptatum nam odio sed? Architecto illum molestiae incidunt iusto repudiandae tempora odit, excepturi quam porro consequatur quis, pariatur nulla fugiat illo dignissimos dolore aut laborum deleniti est totam repellendus suscipit recusandae libero. Sapiente eum beatae nobis pariatur praesentium? Architecto nostrum dolores iusto quo hic nemo suscipit similique maiores pariatur consequuntur. Deserunt dolorum nulla reiciendis repudiandae! Tempore soluta asperiores voluptatem reiciendis incidunt fugit libero ipsum dignissimos, excepturi aliquid, cupiditate, eum delectus tenetur quae necessitatibus inventore facilis expedita atque explicabo. Inventore dolorum nesciunt harum dolore.'
+        ]);
     }
 }
