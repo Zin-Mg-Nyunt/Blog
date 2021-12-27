308d352b983a04075704113716b41b45c770c639
ZinMgNyunt <zinmgnyunt99@gmail.com>
2021-12-27 21:25:23 +0630

    episode-36 Design integration with components extract

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,20 +1,9 @@
-<x-layout> 
-    <x-slot name='title'> 
-        <title>All Blogs</title>
-    </x-slot>
-    @foreach($blogs as $blog)
-        <div class="blog">
-            <h1><a href="/blogs/{{ $blog->slug }}"> {{  $blog->title }} </a></h1>
-            <a href="/categories/{{ $blog->category->slug }}">
-                {{ $blog->category->name }}
-            </a>
-            <h4>Author - <a href="/users/{{ $blog->author->username }}">{{ $blog->author->name }}</a></h4>
-            <div>
-                <p>Published at - {{  $blog->created_at->diffForHumans() }}</p>
-                <p>{{ $blog->intro }}</p>
-                <p>Updated at - {{ $blog->updated_at->diffForHumans() }}</p>
-            </div>
-        </div>
-    @endforeach
+<x-layout>
+    <!-- hero section -->
+    <x-hero />
+    <!-- blogs section -->
+    <x-blog-section />
+    <!-- subscribe new blogs -->
+    <x-subscribe />
 </x-layout>
 
