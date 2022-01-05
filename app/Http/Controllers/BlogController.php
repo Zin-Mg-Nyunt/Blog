ceabdb557a56278dc7fd197db9cb511faf69ef9d
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-01-05 23:49:48 +0630

    episode-44 when and scopeFilter(query-scope)

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,19 +8,36 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    protected function getBlogs()//query ကို method တစ်ခုအနေနဲ့ ခွဲထုတ်ရေးလိုက်
+    protected function getBlogs()
     {
-        $blogs=Blog::latest();
-        if (request('search')) {
-            $blogs=$blogs->where('title', 'LIKE', '%'.request('search').'%')
-                        ->orWhere('body', 'LIKE', '%'.request('search').'%');
-        }
-        return $blogs->get();//return ပြန်တဲ့အခါ get()နဲ့ပါ blog ကိုဆွဲထုတ်ပြီးတော့မှ return ပြန်
+        return Blog::latest()->filter()->get();
+        //filter() ဆိုတဲ့ method က eloquent model(Blog) ရဲ့ method မဟုတ်ဘူး။အဲ့တော့ custom method တစ်ခုအနေနဲ့ရေးရတယ်။အဲ့လိုရေးဖို့ scopequery ရေးနည်းနဲ့ရေးရတယ်
+
+        // $query=Blog::latest();//ဒါက blogs ကိုဆွဲထုတ်တယ်ဆိုတာထက် query တစ်ခုကိုပြင်ဆင်တာပဲရှိသေးတယ်။ ဒါကြောင့် $query လို့ variable ကိုသတ်မှတ်မယ်
+
+        // // ဒါက ရိုးရိုး if else ပုံစံနဲ့ရေးထားတာ
+        // // if (request('search')) {
+        // //     $blogs=$blogs->where('title', 'LIKE', '%'.request('search').'%')
+        // //                 ->orWhere('body', 'LIKE', '%'.request('search').'%');
+        // // }
+
+        // // ဒါက OOP ပုံစံနဲ့ရေးတာ
+        // $query->when(request('search'), function ($query, $search) {
+        //     $query->where('title', 'LIKE', '%'.$search.'%')
+        //         ->orWhere('body', 'LIKE', '%'.$search.'%');
+        // });
+        // // if နဲ့စစ်မဲ့အစား when ဆိုတဲ့ laravel ကလုပ်ပေးတဲ့ Eloquent Model Method နဲ့စစ်မယ်။ when မှာ parameter နှစ်ခု လက်ခံမယ်။ First parameter က true/အမှန်တရား/positive ဘက်ကိုသွားတယ်ဆိုရင် Second parameter ဖြစ်တဲ့ callback function ကအလုပ်လုပ်မယ်။ callback function ထဲမှာ parameter နှစ်ခုလက်ခံမယ်။ First Parameter က လက်ရှိ run နေတဲ့ query[Blog::latest()] ဆိုတဲ့ဟာကိုလက်ခံမယ်။ Second Parameter  က when ရဲ့ first parameter ဖြစ်တဲ့ (search value) ကိုလက်ခံမယ်။
+
+        // return $query->get();
     }
     public function index()
     {
         return view('blogs', [
-                'blogs'=>$this->getBlogs(),//query ထုတ်ရေးထားတဲ့ method name ကိုခေါ်ပြီးပြန်သုံး
+            //ဒီ Blog::latest()->filter()->get() ဆိုတဲ့ query လေးကတိုလို့ method တစ်ခုခွဲရေးနေစရာမလိုတော့ဘူး
+            //filter method ကနေ data ထည့်ပေးလို့ရတယ်။စစ်မယ့် condition ကိုလဲထည့်ပေးလို့ရတယ်
+            //request() ထဲက search က string ပုံစံဖြစ်နေလို့ array ပုံစံပြောင်းမယ်။ search ကို array[] လေးထောင့်ကွင်းလေး အုပ်လိုက်ရုံပဲ
+            //array ပုံစံနဲ့ရေးထားတော့ ကြိုက်တာနဲ့ စစ်လို့ရသွားပြီ။ author နဲ့ပဲစစ်စစ်၊ category နဲ့ပဲစစ်စစ်
+                'blogs'=>Blog::latest()->filter(request(['search']))->get(),
                 'categories'=>Category::all()
             ]);
     }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -11,6 +11,20 @@ class Blog extends Model
     protected $guarded=['id'];
     protected $with=['category','author'];//$with[] ထဲမှာ ထည့်ထားတဲ့ဟာက relationship တွေ(method တွေ)။ အဲ့လိုရေးထားလိုက်ရင် web.php မှာ with() တွေ load() တွေနဲ့ eager load ပုံစံတွေရေးစရာမလိုတော့ဘူး
 
+    // scopequery ရေးနည်း
+    //scopeFilter ရဲ့ first parameter ထဲမှာ filter method ကိုခေါ်ထားတဲ့ query[Blog::latest()] ကို ထည့်ပေးရတယ်
+    //scopeFilter ရဲ့ second parameter ထဲမှာ filter method ကပို့ပေးလိုက်တဲ့ data ကိုလက်ခံလို့ရတယ်
+    //array ပုံစံနဲ့ရေးထားတော့ ကြိုက်တာနဲ့ စစ်လို့ရသွားပြီ။ author နဲ့ပဲစစ်စစ်၊ category နဲ့ပဲစစ်စစ်
+    public function scopeFilter($query, $filter)//Blog::latest()->filter()
+    {
+        //$filter['search']??false ဆိုတာက $filter['search'] ဆိုတဲ့ထဲမှာ search data ရှိရင် အဲ့search data ကိုထည့်ပြီး မရှိရင် false ကိုထည့်(terniary operator ကို အတိုကောက်ရေးတဲ့ပုံစံ)။ false ဝင်သွားရင် second parameter ထဲက callback function ကအလုပ်လုပ်စရာမလိုတော့ဘူး
+        //ဘယ်လိုအချိန်မှာ search data မရှိဘူးလဲဆိုရင် website ထဲကို ဝင်ခါစ အချိန်မှာဆိုရင် ဘာsearch data မှမရှိဘူး အဲ့လိုအချိန်ဆိုရင် when ရဲ့ first parameter ထဲကို false ကဝင်သွားပြီ အနောက်က callback function ကအလုပ်လုပ်စရာမလိုတော့ဘူး
+        $query->when($filter['search']??false, function ($query, $search) {
+            $query->where('title', 'LIKE', '%'.$search.'%')
+                ->orWhere('body', 'LIKE', '%'.$search.'%');
+        });
+    }
+
     //Eloquent Model Relationship သုံးဖို့ လိုတဲ့ method
     public function category()
     {
