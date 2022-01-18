8bd04bd46411db45e14255bc36de0e5eeeca93ae
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-01-18 21:41:19 +0630

    episode-53 registration design integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Blog;
@@ -9,24 +10,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 Route::get('/', [BlogController::class,'index']);
-
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show']);
 
-//File Naming Conventions
-//လုပ်ဆောင်ချက် -> controller method -> view file နာမည်(feature.method)
-//all -> index -> blogs.index
-//single -> show -> blogs.show
-//create form -> create -> blogs.create
-//server store -> store -> -
-//edit form -> edit -> blogs.edit
-//server update -> update -> -
-//server delete -> destroy -> -
-
-//Students Project
-//all -> index -> students.index
-//single -> show -> students.show
-//create form -> create -> students.create
-//server store -> store -> -
-//edit form -> edit -> students.edit
-//server update -> update -> -
-//server delete -> destroy -> -
+Route::get('/register', [AuthController::class,'create']);
