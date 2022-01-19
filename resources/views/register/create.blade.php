66d91fcf5a62abb3812674785fcf79ec543f34e3
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-01-19 21:57:00 +0630

    episode-54 post method and CSRF concept

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,22 +4,23 @@
             <div class="col-md-4 mx-auto">
                 <h3 class="text-primary text-center my-2">Register Form</h3>
                 <div class="card p-4 my-3 shadow-sm">
-                    <form>
+                    <form method="POST" action="/register">
+                        @csrf
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Name</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Username</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                           </div>
                         <div class="mb-3">
                           <label for="exampleInputPassword1" class="form-label">Password</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1">
+                          <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                       </form>
