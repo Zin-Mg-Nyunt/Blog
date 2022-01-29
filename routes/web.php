95ecd9b360d3fd5825019551f5335b22001c7e3d
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-01-29 20:44:57 +0630

    episode-62 middleware

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,13 @@ use Illuminate\Support\Facades\Log;
 Route::get('/', [BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show']);
 
-Route::get('/register', [AuthController::class,'create']);
-Route::post('/register', [AuthController::class,'store']);
-Route::post('/logout', [AuthController::class,'logout']);
-Route::get('/login', [AuthController::class,'login']);
-Route::post('/login', [AuthController::class,'post_login']);
+//register route ကို guestဖြစ်နေတယ့်အချိန်၊ login မဝင်ရသေးတဲ့အချိန် မှသာ ဝင်လို့ရမယ်
+Route::get('/register', [AuthController::class,'create'])->middleware('guest');
+Route::post('/register', [AuthController::class,'store'])->middleware('guest');
+
+//logout route ကို authဖြစ်နေတယ့်အချိန်၊ login ဝင်ပြီးတဲ့အချိန် မှသာ ဝင်လို့ရမယ်
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth');
+
+//login route ကို guestဖြစ်နေတယ့်အချိန်၊ login မဝင်ရသေးတဲ့အချိန် မှသာ ဝင်လို့ရမယ်
+Route::get('/login', [AuthController::class,'login'])->middleware('guest');
+Route::post('/login', [AuthController::class,'post_login'])->middleware('guest');
