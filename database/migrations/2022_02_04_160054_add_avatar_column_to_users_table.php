d5bf6c501c9fcc709e0083480f848a32ebb9ea03
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-02-04 23:20:41 +0630

    episode-66 add table column in production level

---
+++ database/migrations/2022_02_04_160054_add_avatar_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAvatarColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable()->after('username');
+            //username column ပြီးရင် avatar column လာစေချင်ရင် => after()
+            //nullable() => data မထည့်လဲရတယ်
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+        });
+    }
+}
