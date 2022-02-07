04b1d8069dc2dd680005695a3e8fe8c140dc3a46
ZinMgNyunt <zinmgnyunt99@gmail.com>
2022-02-08 00:29:05 +0630

    episode-68 work with component attribute

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,4 +1,17 @@
-
+<section class="container">
+    <div class="col-md-8 mx-auto">
+        <x-card-wrapper class="bg-secondary">
+            <form>
+                <div class="mb-3">
+                    <textarea class="form-control" cols="10" rows="5" placeholder="comment here..."></textarea>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </div>
+            </form>
+        </x-card-wrapper>
+    </div>
+</section>
 <section class="container">
     <div class="col-md-8  mx-auto">
         <h5 class="my-3 text-secondary">Comments {{ count($comments) }}</h5>
